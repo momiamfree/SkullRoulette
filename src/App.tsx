ad65297a2a3caa773fa870b8c7372afa301d21a0
Skullkid <momiamfree@gmail.com>
2026-02-08 13:59:20 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Roulette from "./Roulette";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center gap-8">
+      <Roulette />
+    </div>
+  );
+}
